9b06de62187d20ba3c1f19679506f53ab14b9e10
Marc Bouvier <m.bouvier.dev@gmail.com>
2019-03-11 20:54:21 +0100

    lien vers github

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,6 +3,7 @@
     <g-image alt="Example image" src="~/favicon.png" width="135" />
     <h1>Hello, world!</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores</p>
+    <footer><a href="https://github.com/marc-bouvier/baldir.fr">baldir.fr sur GitHub</a></footer>
   </Layout>
 </template>
 
